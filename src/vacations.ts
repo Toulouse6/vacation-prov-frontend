89c9e50dfa7a038648f0477899da99cd4a013960
Touluse <tal.argamanbib@gmail.com>
2024-11-18 15:16:39 +0200

    description

--- src/vacations.ts
+++ src/vacations.ts
@@ -2,25 +2,16 @@ const vacations = [
     {
         id: 1,
         destination: "Amsterdam",
-        description: "Explore the vibrant canals, historic buildings, and lively atmosphere of Amsterdam city.",
+        description: "Explore the vibrant canals, historic buildings, and lively atmosphere of Amsterdam city. A perfect mix of charm and culture awaits you.",
         startDate: "2024-06-01",
         endDate: "2024-06-07",
         price: 1200.0,
         imageUrl: process.env.PUBLIC_URL + "/assets/vacations/amsterdam.png"
     },
     {
-        id: 2,
-        destination: "Jerusalem",
-        description: "Discover the ancient streets, spiritual landmarks, and rich culture of Jerusalem's history.",
-        startDate: "2024-11-18",
-        endDate: "2024-12-29",
-        price: 1100.0,
-        imageUrl: process.env.PUBLIC_URL + "/assets/vacations/jerusalem.png"
-    },
-    {
         id: 3,
         destination: "Miami",
-        description: "Relax on Miami's glamorous beaches, enjoy the nightlife, and soak in its sunny tropical vibes.",
+        description: "Relax on Miami's glamorous beaches, enjoy the nightlife, and soak in its sunny tropical vibes. Let the energy of Miami captivate you.",
         startDate: "2024-06-20",
         endDate: "2024-06-25",
         price: 900.0,
@@ -29,7 +20,7 @@ const vacations = [
     {
         id: 4,
         destination: "Samui",
-        description: "Unwind on Koh Samui's pristine beaches, surrounded by lush greenery and clear blue waters.",
+        description: "Unwind on Koh Samui's pristine beaches, surrounded by lush greenery and clear blue waters. Indulge in the ultimate island paradise.",
         startDate: "2024-07-01",
         endDate: "2024-07-10",
         price: 950.0,
@@ -38,7 +29,7 @@ const vacations = [
     {
         id: 5,
         destination: "Tokyo",
-        description: "Immerse yourself in Tokyo's bustling streets, futuristic skyline, and rich cultural traditions.",
+        description: "Immerse yourself in Tokyo's bustling streets, futuristic skyline, and rich cultural traditions. Discover a city where innovation meets history.",
         startDate: "2024-07-15",
         endDate: "2024-07-22",
         price: 1500.0,
@@ -47,25 +38,16 @@ const vacations = [
     {
         id: 6,
         destination: "Athens",
-        description: "Step into history as you explore Athens' ancient ruins, vibrant markets, and classic landmarks.",
+        description: "Step into history as you explore Athens' ancient ruins, vibrant markets, and classic landmarks. The cradle of Western civilization awaits you.",
         startDate: "2024-08-01",
         endDate: "2024-08-10",
         price: 1200.0,
         imageUrl: process.env.PUBLIC_URL + "/assets/vacations/athens.png"
     },
     {
-        id: 7,
-        destination: "Kenya",
-        description: "Embark on a thrilling Kenyan safari to witness breathtaking wildlife and serene landscapes.",
-        startDate: "2024-11-12",
-        endDate: "2024-12-27",
-        price: 2000.0,
-        imageUrl: process.env.PUBLIC_URL + "/assets/vacations/kenya.png"
-    },
-    {
         id: 8,
         destination: "Barcelona",
-        description: "Admire Gaudi's masterpieces, sunny beaches, and lively streets in the heart of Barcelona.",
+        description: "Admire Gaudi's masterpieces, sunny beaches, and lively streets in the heart of Barcelona. Discover a city full of color and life.",
         startDate: "2024-09-01",
         endDate: "2024-09-10",
         price: 1300.0,
@@ -74,7 +56,7 @@ const vacations = [
     {
         id: 9,
         destination: "Lisbon",
-        description: "Wander through Lisbon's colorful streets, historic sites, and breathtaking ocean views.",
+        description: "Wander through Lisbon's colorful streets, historic sites, and breathtaking ocean views. Enjoy the unique charm of this Portuguese gem.",
         startDate: "2024-09-15",
         endDate: "2024-09-22",
         price: 1100.0,
@@ -83,7 +65,7 @@ const vacations = [
     {
         id: 10,
         destination: "Petra",
-        description: "Discover the ancient city of Petra, carved into red rock, and full of fascinating history.",
+        description: "Discover the ancient city of Petra, carved into red rock, and full of fascinating history. Marvel at its timeless beauty and mystery.",
         startDate: "2024-09-30",
         endDate: "2024-10-09",
         price: 1600.0,
@@ -92,7 +74,7 @@ const vacations = [
     {
         id: 11,
         destination: "Sydney",
-        description: "Enjoy Sydney's famous Opera House, golden beaches, and vibrant city life in sunny Australia.",
+        description: "Enjoy Sydney's famous Opera House, golden beaches, and vibrant city life in sunny Australia. Let Sydney's charm take your breath away.",
         startDate: "2024-10-14",
         endDate: "2024-10-24",
         price: 1700.0,
@@ -101,7 +83,7 @@ const vacations = [
     {
         id: 12,
         destination: "Berlin",
-        description: "Dive into Berlin's dynamic art scene, historic sites, and energetic, modern culture in Germany.",
+        description: "Dive into Berlin's dynamic art scene, historic sites, and energetic, modern culture in Germany. Experience a city that redefines creativity.",
         startDate: "2024-10-31",
         endDate: "2024-11-09",
         price: 1400.0,
@@ -110,38 +92,56 @@ const vacations = [
     {
         id: 13,
         destination: "London",
-        description: "Explore London's iconic landmarks, rich history, and diverse neighborhoods full of charm.",
+        description: "Explore London's iconic landmarks, rich history, and diverse neighborhoods full of charm. Discover a city where tradition meets innovation.",
         startDate: "2024-11-14",
         endDate: "2024-11-21",
         price: 1500.0,
         imageUrl: process.env.PUBLIC_URL + "/assets/vacations/london.png"
     },
     {
+        id: 7,
+        destination: "Kenya",
+        description: "Embark on a thrilling Kenyan safari to witness breathtaking wildlife and serene landscapes. Get closer to nature like never before.",
+        startDate: "2024-11-12",
+        endDate: "2024-12-27",
+        price: 2000.0,
+        imageUrl: process.env.PUBLIC_URL + "/assets/vacations/kenya.png"
+    },
+    {
+        id: 2,
+        destination: "Jerusalem",
+        description: "Discover the ancient streets, spiritual landmarks, and rich culture of Jerusalem's history. Walk through time in this sacred city.",
+        startDate: "2024-11-18",
+        endDate: "2024-12-29",
+        price: 1100.0,
+        imageUrl: process.env.PUBLIC_URL + "/assets/vacations/jerusalem.png"
+    },
+    {
         id: 14,
         destination: "Phangan",
-        description: "Experience Koh Phangan's stunning beaches, serene environment, and world-famous full-moon party.",
+        description: "Experience Koh Phangan's stunning beaches, serene environment, and world-famous full-moon party. Relax and let the island vibes inspire you.",
         startDate: "2024-11-30",
         endDate: "2024-12-09",
         price: 900.0,
         imageUrl: process.env.PUBLIC_URL + "/assets/vacations/phangan.png"
     },
     {
-        id: 15,
-        destination: "Tel Aviv",
-        description: "Enjoy Tel Aviv's bustling beaches, vibrant nightlife, and rich blend of culture and history.",
-        startDate: "2025-02-14",
-        endDate: "2025-03-11",
-        price: 1300.0,
-        imageUrl: process.env.PUBLIC_URL + "/assets/vacations/tel-aviv.png"
-    },
-    {
         id: 16,
         destination: "Seoul",
-        description: "Discover Seoul's mix of modern skyscrapers, ancient palaces, and unique culinary delights.",
+        description: "Discover Seoul's mix of modern skyscrapers, ancient palaces, and unique culinary delights. Explore Korea's vibrant and rich culture.",
         startDate: "2025-01-01",
         endDate: "2025-01-10",
         price: 1100.0,
         imageUrl: process.env.PUBLIC_URL + "/assets/vacations/seoul.png"
+    },
+    {
+        id: 15,
+        destination: "Tel Aviv",
+        description: "Enjoy Tel Aviv's bustling beaches, vibrant nightlife, and rich blend of culture and history. A perfect destination for fun and exploration.",
+        startDate: "2025-02-14",
+        endDate: "2025-03-11",
+        price: 1300.0,
+        imageUrl: process.env.PUBLIC_URL + "/assets/vacations/tel-aviv.png"
     }
 ];
 
